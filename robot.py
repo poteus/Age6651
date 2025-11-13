51b33b8de72b7fdfa707ebfdf2a963f8ed3c5f2c
dan.j02 <iam.julio160@gmail.com>
2025-11-13 10:07:45 -0600

    Robotpy updates, Added class robot

--- robot.py
+++ robot.py
@@ -1,1 +1,44 @@
 # TODO make robot class
+
+import wpilib
+import wpilib.drive
+import phoenix6
+
+class robot(wpilib.TimedRobot):
+    def robotInit(self):
+        #init drive motors
+
+        self.leftDrive = phoenix6.hardware.TalonFX(1)
+        self.rightDrive = phoenix6.hardware.TalonFX(2)
+        self.rightDrive.setInverted(True)
+        self.robotDrive = wpilib.drive.DifferentialDrive(self.leftDrive, self.rightDrive)
+        
+        self.controller = wpilib.XboxController(0)
+        
+        self.timer = wpilib.Timer() 
+
+    def autoInit(self):
+        #prepare for auto
+        self.timer.restart()
+
+    def autoPeriodic(self):
+        #autonomous movement
+        if self.timer.get() < 2.0:
+            self.robotDrive.arcadeDrive(.5, 0, False)
+        else:
+            self.robotDrive.stopMotor()
+
+    def teleopInit(self):
+        #init for teleop
+        pass
+
+    def teleopPeriodic(self):
+        #teleop loop
+        self.robotDrive.arcadeDrive(
+            -self.controller.getLeftY(), -self.controller.getRightX()
+        )
+
+    def testInit(self):
+        pass
+    def testPeriodic(self):
+        pass
