4a976154141d804e1fb7703182ac2876454276e4
Alexis Rivera <arivera@sps5.org>
2025-12-10 10:31:52 -0600

    Serve Drive Created

--- robot.py
+++ robot.py
@@ -1,4 +1,3 @@
-        pass
 #!/usr/bin/env python3
 #
 # Copyright (c) FIRST and other WPILib contributors.
@@ -6,84 +5,75 @@
 # the WPILib BSD license file in the root directory of this project.
 #
 
-import typing
-
 import wpilib
 import commands2
-import commands2.cmd
-import robotcontainer
+import typing
 
-"""
-The VM is configured to automatically run this class, and to call the functions corresponding to
-each mode, as described in the TimedRobot documentation. If you change the name of this class or
-the package after creating this project, you must also update the build.gradle file in the
-project.
-"""
+from robotcontainer import RobotContainer
 
 
 class MyRobot(commands2.TimedCommandRobot):
     """
-    Our default robot class, pass it to wpilib.run
     Command v2 robots are encouraged to inherit from TimedCommandRobot, which
     has an implementation of robotPeriodic which runs the scheduler for you
     """
+
+    autonomousCommand: typing.Optional[commands2.Command] = None
 
     def robotInit(self) -> None:
         """
         This function is run when the robot is first started up and should be used for any
         initialization code.
         """
-        self.autonomousCommand: typing.Optional[commands2.Command] = None
-    
+
         # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         # autonomous chooser on the dashboard.
-        self.container = robotcontainer.RobotContainer()
+        self.container = RobotContainer()
 
     def robotPeriodic(self) -> None:
-        self.shuffleboardinfo.updateShuffleboard()
-        return super().robotPeriodic()
+        """This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+        that you want ran during disabled, autonomous, teleoperated and test.
+
+        This runs after the mode specific periodic functions, but before LiveWindow and
+        SmartDashboard integrated updating."""
+
+        # Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        # commands, running already-scheduled commands, removing finished or interrupted commands,
+        # and running subsystem periodic() methods.  This must be called from the robot's periodic
+        # block in order for anything in the Command-based framework to work.
+        commands2.CommandScheduler.getInstance().run()
 
     def disabledInit(self) -> None:
         """This function is called once each time the robot enters Disabled mode."""
+        pass
 
     def disabledPeriodic(self) -> None:
         """This function is called periodically when disabled"""
+        pass
 
     def autonomousInit(self) -> None:
         """This autonomous runs the autonomous command selected by your RobotContainer class."""
-        # self.container.swerveSubsystem.resetEncoder()
         self.autonomousCommand = self.container.getAutonomousCommand()
 
-        # schedule the autonomous command (example)
-        if self.autonomousCommand is not None:
+        if self.autonomousCommand:
             self.autonomousCommand.schedule()
-        else:
-            print("no auto command?")
 
     def autonomousPeriodic(self) -> None:
         """This function is called periodically during autonomous"""
-        self.shuffleboardinfo.updateShuffleboard()
+        pass
 
     def teleopInit(self) -> None:
         # This makes sure that the autonomous stops running when
         # teleop starts running. If you want the autonomous to
         # continue until interrupted by another command, remove
         # this line or comment it out.
-        if self.autonomousCommand is not None:
+        if self.autonomousCommand:
             self.autonomousCommand.cancel()
 
     def teleopPeriodic(self) -> None:
         """This function is called periodically during operator control"""
-        self.shuffleboardinfo.updateShuffleboard()
+        pass
 
     def testInit(self) -> None:
         # Cancels all running commands at the start of test mode
         commands2.CommandScheduler.getInstance().cancelAll()
-
-    def testPeriodic(self) -> None:
-        self.shuffleboardinfo.updateShuffleboard()
-        return super().testPeriodic()
-
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
