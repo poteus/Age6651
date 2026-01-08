0b70f1a40ede2c512fc709dd0b23324fc804b9fc
Alexis Rivera <arivera@sps5.org>
2026-01-08 11:15:43 -0600

    feat: Implement drive motor Torque Current FOC gains

--- generated/Murphy_tuner_constants.py
+++ generated/Murphy_tuner_constants.py
@@ -44,13 +44,22 @@ class TunerConstants:
         .with_k_v(0.122585)
         .with_k_a(0.0042655)
     )
+    _drive_gains = ( # TORQUE_CURRENT_FOC
+        configs.Slot0Configs()
+        .with_k_p(5.0) # Current starts around 5-10 amps
+        .with_k_i(0)
+        .with_k_d(0.1) # A tiny bit of D helps FOC stability
+        .with_k_s(2.5) # It takes ~2-4 amps just to break friction
+        .with_k_v(1.5) # Ballpark Amps per rotation/sec
+        .with_k_a(0.1) # Acceleration constant
+    )
 
     # The closed-loop output type to use for the steer motors;
     # This affects the PID/FF gains for the steer motors
-    _steer_closed_loop_output = swerve.ClosedLoopOutputType.VOLTAGE
+    _steer_closed_loop_output = swerve.ClosedLoopOutputType.TORQUE_CURRENT_FOC
     # The closed-loop output type to use for the drive motors;
     # This affects the PID/FF gains for the drive motors
-    _drive_closed_loop_output = swerve.ClosedLoopOutputType.VOLTAGE
+    _drive_closed_loop_output = swerve.ClosedLoopOutputType.TORQUE_CURRENT_FOC
 
     # The type of motor used for the drive motor
     _drive_motor_type = swerve.DriveMotorArrangement.TALON_FX_INTEGRATED
