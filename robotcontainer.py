0c15046b121b7a327674d56e051910f82f273039
Alexis Rivera <arivera@sps5.org>
2026-01-14 10:43:03 -0600

    feat: Configure RobotContainer for shooter control and drivetrain cleanup

--- robotcontainer.py
+++ robotcontainer.py
@@ -54,7 +54,6 @@ class RobotContainer:
             0
         )  # 3/4 of a rotation per second max angular velocity
 
-        print("With MOTION MAGIC.")
         # Setting up bindings for necessary control of the swerve drive platform
         self._drive = (
             swerve.requests.FieldCentric()
@@ -87,172 +86,30 @@ class RobotContainer:
         instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
         and then passing it to a JoystickButton.
         """
-
-        # Note that X is defined as forward according to WPILib convention,
-        # and Y is defined as to the left according to WPILib convention.
-        self.drivetrain.setDefaultCommand(
-            # Drivetrain will execute this command periodically
-            self.drivetrain.apply_request(
-                lambda: (
-                    self._drive.with_velocity_x(
-                        -self._joystick.getLeftY() * self._max_speed
-                    )  # Drive forward with negative Y (forward)
-                    .with_velocity_y(
-                        -self._joystick.getLeftX() * self._max_speed
-                    )  # Drive left with negative X (left)
-                    .with_rotational_rate(
-                        -self._joystick.getRightX() * self._max_angular_rate
-                    )  # Drive counterclockwise with negative X (left)
-                )
-            )
+        # --- LOGGER CONTROL ---
+        # Press "Start" button to begin logging
+        self._joystick.start().onTrue(
+            commands2.cmd.runOnce(lambda: SignalLogger.start())
+        )
+        # We'll use the Back button for both stopping the logger AND resetting jams
+        self._joystick.back().onTrue(
+            commands2.cmd.runOnce(lambda: SignalLogger.stop())
+        ).onTrue(
+            commands2.cmd.runOnce(lambda: self.shooter.reset_jam())
         )
 
-        # Idle while the robot is disabled. This ensures the configured
-        # neutral mode is applied to the drive motors while disabled.
-        idle = swerve.requests.Idle()
-        Trigger(DriverStation.isDisabled).whileTrue(
-            self.drivetrain.apply_request(lambda: idle).ignoringDisable(True)
-        )
-
-        self._joystick.a().whileTrue(self.drivetrain.apply_request(lambda: self._brake))
-        self._joystick.b().whileTrue(
-            self.drivetrain.apply_request(
-                lambda: self._point.with_module_direction(
-                    Rotation2d(-self._joystick.getLeftY(), -self._joystick.getLeftX())
-                )
-            )
-        )
-
-        # Run SysId routines when holding back/start and X/Y.
-        # Note that each routine should be run exactly once in a single log.
-        (self._joystick.back() & self._joystick.y()).whileTrue(
-            self.drivetrain.sys_id_dynamic(SysIdRoutine.Direction.kForward)
-        )
-        (self._joystick.back() & self._joystick.x()).whileTrue(
-            self.drivetrain.sys_id_dynamic(SysIdRoutine.Direction.kReverse)
-        )
-        (self._joystick.start() & self._joystick.y()).whileTrue(
-            self.drivetrain.sys_id_quasistatic(SysIdRoutine.Direction.kForward)
-        )
-        (self._joystick.start() & self._joystick.x()).whileTrue(
-            self.drivetrain.sys_id_quasistatic(SysIdRoutine.Direction.kReverse)
-        )
-
-        # Start and stop SignalLogger with the bumpers
-        self._joystick.leftBumper().onTrue(
-            self.shooter.run_shooter(11.0) 
-        )
-
-    def getAutonomousCommand(self) -> commands2.Command:
-        """Use this to pass the autonomous command to the main {@link Robot} class.
-
-        :returns: the command to run in autonomous
-        """
-        return commands2.cmd.print_("No autonomous command configured")
-=======
-#
-# Copyright (c) FIRST and other WPILib contributors.
-# Open Source Software; you can modify and/or share it under the terms of
-# the WPILib BSD license file in the root directory of this project.
-#
-
-import commands2
-import commands2.cmd
-from commands2.button import CommandXboxController, Trigger
-
-from generated.tuner_constants import TunerConstants
-from telemetry import Telemetry
-
-from phoenix6 import swerve
-from wpilib import DriverStation
-from wpimath.geometry import Rotation2d
-from wpimath.units import rotationsToRadians
+        # --- Characterization Bindings ---
+        # Quasistatic: Motor ramps up speed slowly
+        self._joystick.y().whileTrue(self.shooter.sys_id_quasistatic(SysIdRoutine.Direction.kForward))
+        self._joystick.x().whileTrue(self.shooter.sys_id_quasistatic(SysIdRoutine.Direction.kReverse))
+        
+        # Dynamic: Motor gets a sudden 7V "step"
+        self._joystick.b().whileTrue(self.shooter.sys_id_dynamic(SysIdRoutine.Direction.kForward))
+        self._joystick.a().whileTrue(self.shooter.sys_id_dynamic(SysIdRoutine.Direction.kReverse))
 
 
-class RobotContainer:
-    """
-    This class is where the bulk of the robot should be declared. Since Command-based is a
-    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
-    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-    subsystems, commands, and button mappings) should be declared here.
-    """
-
-    def __init__(self) -> None:
-        self._max_speed = (
-            TunerConstants.speed_at_12_volts
-        )  # speed_at_12_volts desired top speed
-        self._max_angular_rate = rotationsToRadians(
-            0.75
-        )  # 3/4 of a rotation per second max angular velocity
-
-        # Setting up bindings for necessary control of the swerve drive platform
-        self._drive = (
-            swerve.requests.FieldCentric()
-            .with_deadband(self._max_speed * 0.1)
-            .with_rotational_deadband(
-                self._max_angular_rate * 0.1
-            )  # Add a 10% deadband
-            .with_drive_request_type(
-                swerve.SwerveModule.DriveRequestType.OPEN_LOOP_VOLTAGE
-            )  # Use open-loop control for drive motors
-        )
-        self._brake = swerve.requests.SwerveDriveBrake()
-        self._point = swerve.requests.PointWheelsAt()
-
-        self._logger = Telemetry(self._max_speed)
-
-        self._joystick = CommandXboxController(0)
-
-        self.drivetrain = TunerConstants.create_drivetrain()
-
-        # Configure the button bindings
-        self.configureButtonBindings()
-
-    def configureButtonBindings(self) -> None:
-        """
-        Use this method to define your button->command mappings. Buttons can be created by
-        instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
-        and then passing it to a JoystickButton.
-        """
-
-        # Note that X is defined as forward according to WPILib convention,
-        # and Y is defined as to the left according to WPILib convention.
-        self.drivetrain.setDefaultCommand(
-            # Drivetrain will execute this command periodically
-            self.drivetrain.apply_request(
-                lambda: (
-                    self._drive.with_velocity_x(
-                        -self._joystick.getLeftY() * self._max_speed
-                    )  # Drive forward with negative Y (forward)
-                    .with_velocity_y(
-                        -self._joystick.getLeftX() * self._max_speed
-                    )  # Drive left with negative X (left)
-                    .with_rotational_rate(
-                        -self._joystick.getRightX() * self._max_angular_rate
-                    )  # Drive counterclockwise with negative X (left)
-                )
-            )
-        )
-
-        # Idle while the robot is disabled. This ensures the configured
-        # neutral mode is applied to the drive motors while disabled.
-        idle = swerve.requests.Idle()
-        Trigger(DriverStation.isDisabled).whileTrue(
-            self.drivetrain.apply_request(lambda: idle).ignoringDisable(True)
-        )
-
-        self._joystick.a().whileTrue(self.drivetrain.apply_request(lambda: self._brake))
-        self._joystick.b().whileTrue(
-            self.drivetrain.apply_request(
-                lambda: self._point.with_module_direction(
-                    Rotation2d(-self._joystick.getLeftY(), -self._joystick.getLeftX())
-                )
-            )
-        )
-
-        self.drivetrain.register_telemetry(
-            lambda state: self._logger.telemeterize(state)
-        )
+        self._joystick.rightTrigger().whileTrue(self.shooter.run_command(self.shooter.run_shooter_pid)
+                                            ).onFalse(self.shooter.run_command(self.shooter.stop_shooter))
 
     def getAutonomousCommand(self) -> commands2.Command:
         """Use this to pass the autonomous command to the main {@link Robot} class.
