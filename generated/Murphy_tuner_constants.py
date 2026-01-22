b1aa3f9e46839ce2e0e1184cf40e16fc0f552e5b
Alexis Rivera <arivera@sps5.org>
2026-01-22 15:04:08 -0600

    refactor: Tune swerve drive characterization constants

--- generated/Murphy_tuner_constants.py
+++ generated/Murphy_tuner_constants.py
@@ -49,7 +49,7 @@ class TunerConstants:
         .with_k_p(5.06) # Current starts around 5-10 amps
         .with_k_i(0)
         .with_k_d(0.15) # A tiny bit of D helps FOC stability
-        .with_k_s(3.26) # It takes ~2-4 amps just to break friction
+        .with_k_s(1.0) # 3.26) # It takes ~2-4 amps just to break friction
         .with_k_v(2.19) # Ballpark Amps per rotation/sec
         .with_k_a(0.04) # Acceleration constant
     )
@@ -94,7 +94,7 @@ class TunerConstants:
     # Theoretical free speed (m/s) at 12 V applied output;
     # This needs to be tuned to your individual robot
     # speed_at_12_volts: units.meters_per_second = 4.73
-    speed_at_12_volts: units.meters_per_second = 1.5
+    speed_at_12_volts: units.meters_per_second = 4.0
 
     # Every 1 rotation of the azimuth results in _couple_ratio drive motor turns;
     # This may need to be tuned to your individual robot
