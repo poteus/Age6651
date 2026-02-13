d5783ea1adc27deea8a8e816c1b62066dc188155
AlanP39 <jasonmcgee4039@gmail.com>
2026-02-13 15:50:27 -0600

    Added subsystem indexer to main branch

--- robotcontainer.py
+++ robotcontainer.py
@@ -14,6 +14,7 @@ from telemetry import Telemetry
 
 from phoenix6 import swerve, SignalLogger
 from subsystems.vision import Vision
+from subsystems.indexer import Indexer
 
 import wpilib
 from wpilib import DriverStation
@@ -45,6 +46,11 @@ class RobotContainer:
             #self.vision = Vision(["limelight-front"])
             from generated.Murphy_tuner_constants import TunerConstants 
 
+        # Subsystems
+        self.drivetrain = TunerConstants.create_drivetrain()
+        self.indexer = Indexer()
+
+        # Constants
         self._max_speed = (
             TunerConstants.speed_at_12_volts
         )  # speed_at_12_volts desired top speed
@@ -71,8 +77,6 @@ class RobotContainer:
         self._logger = Telemetry(self._max_speed)
 
         self._joystick = CommandXboxController(0)
-
-        self.drivetrain = TunerConstants.create_drivetrain()
 
         # Configure the button bindings
         self.configureButtonBindings()
@@ -142,117 +146,6 @@ class RobotContainer:
         self._joystick.rightBumper().onFalse(
              commands2.cmd.runOnce(
                 lambda: SignalLogger.stop()))
-
-        self.drivetrain.register_telemetry(
-            lambda state: self._logger.telemeterize(state)
-        )
-
-    def getAutonomousCommand(self) -> commands2.Command:
-        """Use this to pass the autonomous command to the main {@link Robot} class.
-
-        :returns: the command to run in autonomous
-        """
-        return commands2.cmd.print_("No autonomous command configured")
-=======
-#
-# Copyright (c) FIRST and other WPILib contributors.
-# Open Source Software; you can modify and/or share it under the terms of
-# the WPILib BSD license file in the root directory of this project.
-#
-
-import commands2
-import commands2.cmd
-from commands2.button import CommandXboxController, Trigger
-
-from generated.tuner_constants import TunerConstants
-from telemetry import Telemetry
-
-from phoenix6 import swerve
-from wpilib import DriverStation
-from wpimath.geometry import Rotation2d
-from wpimath.units import rotationsToRadians
-
-
-class RobotContainer:
-    """
-    This class is where the bulk of the robot should be declared. Since Command-based is a
-    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
-    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-    subsystems, commands, and button mappings) should be declared here.
-    """
-
-    def __init__(self) -> None:
-        self._max_speed = (
-            TunerConstants.speed_at_12_volts
-        )  # speed_at_12_volts desired top speed
-        self._max_angular_rate = rotationsToRadians(
-            0.75
-        )  # 3/4 of a rotation per second max angular velocity
-
-        # Setting up bindings for necessary control of the swerve drive platform
-        self._drive = (
-            swerve.requests.FieldCentric()
-            .with_deadband(self._max_speed * 0.1)
-            .with_rotational_deadband(
-                self._max_angular_rate * 0.1
-            )  # Add a 10% deadband
-            .with_drive_request_type(
-                swerve.SwerveModule.DriveRequestType.OPEN_LOOP_VOLTAGE
-            )  # Use open-loop control for drive motors
-        )
-        self._brake = swerve.requests.SwerveDriveBrake()
-        self._point = swerve.requests.PointWheelsAt()
-
-        self._logger = Telemetry(self._max_speed)
-
-        self._joystick = CommandXboxController(0)
-
-        self.drivetrain = TunerConstants.create_drivetrain()
-
-        # Configure the button bindings
-        self.configureButtonBindings()
-
-    def configureButtonBindings(self) -> None:
-        """
-        Use this method to define your button->command mappings. Buttons can be created by
-        instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
-        and then passing it to a JoystickButton.
-        """
-
-        # Note that X is defined as forward according to WPILib convention,
-        # and Y is defined as to the left according to WPILib convention.
-        self.drivetrain.setDefaultCommand(
-            # Drivetrain will execute this command periodically
-            self.drivetrain.apply_request(
-                lambda: (
-                    self._drive.with_velocity_x(
-                        -self._joystick.getLeftY() * self._max_speed
-                    )  # Drive forward with negative Y (forward)
-                    .with_velocity_y(
-                        -self._joystick.getLeftX() * self._max_speed
-                    )  # Drive left with negative X (left)
-                    .with_rotational_rate(
-                        -self._joystick.getRightX() * self._max_angular_rate
-                    )  # Drive counterclockwise with negative X (left)
-                )
-            )
-        )
-
-        # Idle while the robot is disabled. This ensures the configured
-        # neutral mode is applied to the drive motors while disabled.
-        idle = swerve.requests.Idle()
-        Trigger(DriverStation.isDisabled).whileTrue(
-            self.drivetrain.apply_request(lambda: idle).ignoringDisable(True)
-        )
-
-        self._joystick.a().whileTrue(self.drivetrain.apply_request(lambda: self._brake))
-        self._joystick.b().whileTrue(
-            self.drivetrain.apply_request(
-                lambda: self._point.with_module_direction(
-                    Rotation2d(-self._joystick.getLeftY(), -self._joystick.getLeftX())
-                )
-            )
-        )
 
         self.drivetrain.register_telemetry(
             lambda state: self._logger.telemeterize(state)
---
+++ subsystems/indexer.py
@@ -0,0 +1,134 @@
+import commands2
+import commands2.sysid
+from rev import SparkMax, SparkLowLevel, SparkMaxConfig, ResetMode, PersistMode
+
+from wpilib import SmartDashboard
+from wpimath.units import seconds
+
+from ntcore import NetworkTableInstance
+
+class Indexer(commands2.Subsystem):
+    def __init__(self):
+        super().__init__()
+
+        # Init network table for speed of indexer
+        nt = NetworkTableInstance.getDefault()
+        table = nt.getTable("SmartDashboard")
+
+        # Create a Topic and a Subscriber
+        # This creates the "box" on the dashboard. Defaulting to 0.0 RPS.
+        self.velocity_topic = table.getDoubleTopic("Indexer/IndexerTargetRPS")
+        self.velocity_pub = self.velocity_topic.publish()
+        self.velocity_pub.set(0.0)
+        self.velocity_sub = self.velocity_topic.subscribe(0.0)
+        
+        # We also want to publish the ACTUAL speed so we can compare them
+        self.actual_pub = table.getDoubleTopic("Indexer/IndexerActualRPS").publish()
+
+        # Initialize Motors (Assuming CAN IDs 50 and 51)
+        self.front = SparkMax(51, SparkLowLevel.MotorType.kBrushless)
+        self.back = SparkMax(50, SparkLowLevel.MotorType.kBrushless)
+
+        # Create Configuration
+        # We set the conversion factor to 1/60 to turn RPM into RPS
+        # Velocity = (RPM / 60) = Revolutions per Second
+        config50 = SparkMaxConfig()
+        
+        config50.encoder.velocityConversionFactor(1.0 / 60.0) 
+        config50.encoder.positionConversionFactor(1.0) # 1 rotation = 1 unit
+        config50.inverted(True)
+      
+        # Set PID Gains (Placeholder values - update after tuning)
+        # kS = 0.14395
+        # kV = 0.12408
+        # kA = 0.012706
+        #kP = 0.050463
+        config50.closedLoop.P(0.0001).I(0).D(0).velocityFF(0.12408) # kV
+        
+        # Velocity = (RPM / 60) = Revolutions per Second
+        config51 = SparkMaxConfig()
+        
+        config51.encoder.velocityConversionFactor(1.0 / 60.0) 
+        config51.encoder.positionConversionFactor(1.0) # 1 rotation = 1 unit
+        config51.inverted(False)
+
+        # Set PID Gains (Placeholder values - update after tuning)
+        # kS = 0.47088
+        # kV = 0.13267
+        # kA = 0.0068033
+        #kP = 0.00026339
+        config51.closedLoop.P(0.0001).I(0).D(0).velocityFF(0.13267)
+        
+        # Apply configuration to both motors
+        self.front.configure(
+            config50,
+            ResetMode.kResetSafeParameters, 
+            PersistMode.kPersistParameters)
+        self.back.configure(
+            config51, 
+            ResetMode.kResetSafeParameters, 
+            PersistMode.kPersistParameters)
+
+        self.front_loop = self.front.getClosedLoopController()
+        self.back_loop = self.back.getClosedLoopController()
+        self.front_encoder = self.front.getEncoder()
+        self.back_encoder = self.back.getEncoder()
+
+        # 3. SysId Characterization Routine
+        self.sys_id_routine = commands2.sysid.SysIdRoutine(
+            commands2.sysid.SysIdRoutine.Config(
+                rampRate=1.0, # 1V per second
+                stepVoltage=7.0,      # 7V for dynamic test
+                timeout=seconds(50)
+            ),
+            commands2.sysid.SysIdRoutine.Mechanism(
+                # Drive both motors at the same voltage
+                lambda volts: (
+                    self.front.setVoltage(volts),
+                    self.back.setVoltage(volts)
+                ),
+                # Log BOTH motors as separate "log.motor" entries
+                lambda log: (
+                    log.motor("front-rollers")
+                        .voltage(self.front.getAppliedOutput() * self.front.getBusVoltage())
+                        .position(self.front.getEncoder().getPosition())
+                        .velocity(self.front.getEncoder().getVelocity()),
+                    log.motor("back-rollers")
+                        .voltage(self.back.getAppliedOutput() * self.back.getBusVoltage())
+                        .position(self.back.getEncoder().getPosition())
+                        .velocity(self.back.getEncoder().getVelocity())
+                ),
+                self
+            )
+        )
+
+    def set_velocity(self, rps: float):
+        """Sets the indexer speed in Revolutions per Second."""
+        self.front_loop.setReference(rps, SparkMax.ControlType.kVelocity)
+        back_target = rps * (7.0 / 5.0)
+        self.back_loop.setReference(back_target, SparkMax.ControlType.kVelocity)
+
+    def stop(self):
+        self.front.stopMotor()
+        self.back.stopMotor()
+
+    def periodic(self):
+        # Log data for debugging
+        SmartDashboard.putNumber("Indexer/Velocity RPS", self.front_encoder.getVelocity())
+        SmartDashboard.putNumber("Indexer/Applied Output", self.front.getAppliedOutput())
+         # Read the value from the Dashboard
+        target_rps = self.velocity_sub.get()
+            
+        # Apply the speed (only if you want it constantly running for testing)
+        # In a real match, you'd use a command, but for bench testing this works:
+        # self.set_velocity(target_rps)
+
+        # Publish the actual velocity for the graph
+        self.actual_pub.set(self.front_encoder.getVelocity())
+
+    # --- SysId Command Factories ---
+    def sysIdQuasistatic(self, direction):
+        return self.sys_id_routine.quasistatic(direction)
+
+    def sysIdDynamic(self, direction):
+        return self.sys_id_routine.dynamic(direction)
